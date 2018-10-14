1f955064779ecbead17d07cb4650efad43879b66
Fernando Gurgel <fag.gurgel@gmail.com>
2018-10-14 04:46:38 -0400

    ...

--- js/custom.js
+++ js/custom.js
@@ -28,10 +28,138 @@ $.getJSON('https://raw.githubusercontent.com/FernandoGurgel/propina-no-more/mast
 })
 
 
+$(function() {
+    // Remove button click
+    $(document).on(
+        'click',
+        '[data-role="dynamic-fields"] > .form-inline [data-role="remove"]',
+        function(e) {
+            e.preventDefault();
+            $(this).closest('.form-inline').remove();
+        }
+    );
+    // Add button click
+    $(document).on(
+        'click',
+        '[data-role="dynamic-fields"] > .form-inline [data-role="add"]',
+        function(e) {
+            e.preventDefault();
+            var container = $(this).closest('[data-role="dynamic-fields"]');
+            new_field_group = container.children().filter('.form-inline:first-child').clone();
+            new_field_group.find('input').each(function(){
+                $(this).val('');
+            });
+            container.append(new_field_group);
+        }
+    );
+});
+
+
+
 $(document).ready(function() {
+
+
+/*
+
+DROP AND DRAW
+  */
+  var obj = $("#dragandrophandler");
+  obj.on('dragenter', function (e) 
+  {
+      e.stopPropagation();
+      e.preventDefault();
+      $(this).css('border', '2px solid #0B85A1');
+  });
+  obj.on('dragover', function (e) 
+  {
+       e.stopPropagation();
+       e.preventDefault();
+  });
+  obj.on('drop', function (e) 
+  {
+   
+       $(this).css('border', '2px dotted #0B85A1');
+       e.preventDefault();
+       var files = e.originalEvent.dataTransfer.files;
+   
+       //We need to send dropped files to Server
+       handleFileUpload(files,obj);
+  });
+  $(document).on('dragenter', function (e) 
+  {
+      e.stopPropagation();
+      e.preventDefault();
+  });
+  $(document).on('dragover', function (e) 
+  {
+    e.stopPropagation();
+    e.preventDefault();
+    obj.css('border', '2px dotted #0B85A1');
+  });
+  $(document).on('drop', function (e) 
+  {
+      e.stopPropagation();
+      e.preventDefault();
+  });
+  
+
+/*
+TABELA COM TABULACAO TRADUCAO
+*/
+
+$(document).ready(function() {
+    var table = $('#dtBasicExample').DataTable();
+ 
+    $('#dtBasicExample tbody').on( 'click', 'tr', function () {
+        if ( $(this).hasClass('selected') ) {
+            $(this).removeClass('selected');
+        }
+        else {
+            table.$('tr.selected').removeClass('selected');
+            $(this).addClass('selected');
+        }
+    } );
+ 
+    $('#button').click( function () {
+        table.row('.selected').remove().draw( false );
+    } );
+} );
+
+  $('#dtBasicExample').DataTable({ 
+    "bLengthChange": false,
+    "oLanguage": {
+      "oAria": {
+          "sSortAscending": ": activate to sort column ascending",
+          "sSortDescending": ": activate to sort column descending"
+      },
+      "oPaginate": {
+          "sFirst": "Primeiro",
+          "sLast": "Último",
+          "sNext": "Próximo",
+          "sPrevious": "Anterior"
+      },
+      "sEmptyTable": "Sem dados disponiveis na tabela",
+      "sInfo": "Exibindo _START_ a _END_ de _TOTAL_ entradas",
+      "sInfoEmpty": "Exibindo 0 a 0 de 0 entradas",
+      "sInfoFiltered": "(filtered from _MAX_ total entries)",
+      "sInfoPostFix": "",
+      "sDecimal": "",
+      "sThousands": ",",
+      "sLengthMenu": "Show _MENU_ entries",
+      "sLoadingRecords": "Carregando...",
+      "sProcessing": "Processando...",
+      "sSearch": "Procurar:",
+      "sSearchPlaceholder": "",
+      "sUrl": "",
+      "sZeroRecords": "Não foi encontrado nenhum dado"
+    }
+  });
+  $('.dataTables_length').addClass('bs-select');
 
   // Smooth scrolling
   $(function() {
+	  
+	         
     $('a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
@@ -146,3 +274,107 @@ $(document).ready(function() {
   });
 
 });
+
+/*
+SISTEMA DRAW AND DROP
+*/
+
+function sendFileToServer(formData,status)
+{
+    var uploadURL ="http://hayageek.com/examples/jquery/drag-drop-file-upload/upload.php"; //Upload URL
+    var extraData ={}; //Extra Data.
+    var jqXHR=$.ajax({
+            xhr: function() {
+            var xhrobj = $.ajaxSettings.xhr();
+            if (xhrobj.upload) {
+                    xhrobj.upload.addEventListener('progress', function(event) {
+                        var percent = 0;
+                        var position = event.loaded || event.position;
+                        var total = event.total;
+                        if (event.lengthComputable) {
+                            percent = Math.ceil(position / total * 100);
+                        }
+                        //Set progress
+                        status.setProgress(percent);
+                    }, false);
+                }
+            return xhrobj;
+        },
+    url: uploadURL,
+    type: "POST",
+    contentType:false,
+    processData: false,
+        cache: false,
+        data: formData,
+        success: function(data){
+            status.setProgress(100);
+ 
+            $("#status1").append("File upload Done<br>");         
+        }
+    }); 
+ 
+    status.setAbort(jqXHR);
+}
+ 
+var rowCount=0;
+function createStatusbar(obj)
+{
+     rowCount++;
+     var row="odd";
+     if(rowCount %2 ==0) row ="even";
+     this.statusbar = $("<div class='statusbar "+row+"'></div>");
+     this.filename = $("<div class='filename'></div>").appendTo(this.statusbar);
+     this.size = $("<div class='filesize'></div>").appendTo(this.statusbar);
+     this.progressBar = $("<div class='progressBar'><div></div></div>").appendTo(this.statusbar);
+     this.abort = $("<div class='abort'>Cancelar</div>").appendTo(this.statusbar);
+     obj.after(this.statusbar);
+ 
+    this.setFileNameSize = function(name,size)
+    {
+        var sizeStr="";
+        var sizeKB = size/1024;
+        if(parseInt(sizeKB) > 1024)
+        {
+            var sizeMB = sizeKB/1024;
+            sizeStr = sizeMB.toFixed(2)+" MB";
+        }
+        else
+        {
+            sizeStr = sizeKB.toFixed(2)+" KB";
+        }
+ 
+        this.filename.html(name);
+        this.size.html(sizeStr);
+    }
+    this.setProgress = function(progress)
+    {       
+        var progressBarWidth =progress*this.progressBar.width()/ 100;  
+        this.progressBar.find('div').animate({ width: progressBarWidth }, 10).html(progress + "% ");
+        if(parseInt(progress) >= 100)
+        {
+            this.abort.hide();
+        }
+    }
+    this.setAbort = function(jqxhr)
+    {
+        var sb = this.statusbar;
+        this.abort.click(function()
+        {
+            jqxhr.abort();
+            sb.hide();
+        });
+    }
+}
+function handleFileUpload(files,obj)
+{
+   for (var i = 0; i < files.length; i++) 
+   {
+        var fd = new FormData();
+        fd.append('file', files[i]);
+ 
+        var status = new createStatusbar(obj); //Using this we can set progress.
+        status.setFileNameSize(files[i].name,files[i].size);
+        sendFileToServer(fd,status);
+ 
+   }
+}
