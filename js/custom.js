2ced9468701a74fef6748a617e5f79fc5178bfea
sergiorobertomao <sergiorobertomao@users.noreply.github.com>
2018-11-29 03:47:21 -0200

    CHEGA !!!!!!

--- js/custom.js
+++ js/custom.js
@@ -143,42 +143,7 @@ $(document).ready(function () {
         });
     }
 
-    /*
     
-    DROP AND DRAW
-      */
-    var obj = $("#dragandrophandler");
-    obj.on('dragenter', function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-        $(this).css('border', '2px solid #0B85A1');
-    });
-    obj.on('dragover', function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-    });
-    obj.on('drop', function (e) {
-
-        $(this).css('border', '2px dotted #0B85A1');
-        e.preventDefault();
-        var files = e.originalEvent.dataTransfer.files;
-
-        //We need to send dropped files to Server
-        handleFileUpload(files, obj);
-    });
-    $(document).on('dragenter', function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-    });
-    $(document).on('dragover', function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-        obj.css('border', '2px dotted #0B85A1');
-    });
-    $(document).on('drop', function (e) {
-        e.stopPropagation();
-        e.preventDefault();
-    });
 
 
     /*
@@ -442,16 +407,31 @@ function createStatusbar(obj) {
         });
     }
 }
-function handleFileUpload(files, obj) {
-    for (var i = 0; i < files.length; i++) {
-        var fd = new FormData();
-        fd.append('file', files[i]);
-
-        var status = new createStatusbar(obj); //Using this we can set progress.
-        status.setFileNameSize(files[i].name, files[i].size);
-        sendFileToServer(fd, status);
-
-    }
-}
 
 
+
+$(function() {
+    // Remove button click
+    $(document).on(
+        'click',
+        '[data-role="dynamic-fields"] > .form-inline [data-role="remove"]',
+        function(e) {
+            e.preventDefault();
+            $(this).closest('.form-inline').remove();
+        }
+    );
+    // Add button click
+    $(document).on(
+        'click',
+        '[data-role="dynamic-fields"] > .form-inline [data-role="add"]',
+        function(e) {
+            e.preventDefault();
+            var container = $(this).closest('[data-role="dynamic-fields"]');
+            new_field_group = container.children().filter('.form-inline:first-child').clone();
+          new_field_group.find('label').html('Upload Document'); new_field_group.find('input').each(function(){
+                $(this).val('');
+            });
+            container.append(new_field_group);
+        }
+    );
+});
